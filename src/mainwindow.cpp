985643537e3a25bdc7dea6f87588a020d79bfbdb
Hiro Protagonist <valentin@boettcher.cf>
2017-03-30 19:36:47 +1300

    A first Try on Serial.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -79,7 +79,7 @@ MainWindow::MainWindow( QWidget * parent ) :
 
     // Serial Monitor
     connect( &comm, &SerialCommunicator::dataRead, this, [this] ( QByteArray data ) {
-        ui->serialMon->insertPlainText( data );
+        ui->serialMon->insertPlainText( QDateTime::toString() + data );
     } );
 
     // Camera selected
