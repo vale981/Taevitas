5f1ed3026631db514aa72f0cdf066e5d64f021aa
Hiro Protagonist <valentin@boettcher.cf>
2017-04-07 16:34:32 +1200

    Test show Err.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -189,7 +189,6 @@ void MainWindow::showError( QString error ) {
     errBox->setParent( this );
     errBox->critical( 0, "Error", "An Error has occured:\n" + error );
     errBox->setFixedSize( 500, 200 );
-    errBox->setWindowModality( Qt::NonModal );
     errBox->setAttribute( Qt::WA_DeleteOnClose, true );
     errBox->show();
 }
